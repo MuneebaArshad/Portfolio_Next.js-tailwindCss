e4d941298f396349f5cb3e863498055c50d33d3b
Muneeba Arshad <alisudhan69@gmail.com>
2024-11-24 02:07:02 +0500

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    eslint: {
+      // Ignores ESLint errors during builds
+      ignoreDuringBuilds: true,
+    },
+  };
+  
+  export default nextConfig;
+  
